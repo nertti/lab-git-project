b39f4c520423313143a504598394a34a7b99fd29
Daniil Neretin <dneretin01@gmail.com>
2025-01-16 23:41:01 +0300

    Add factorial function

--- src/utils.py
+++ src/utils.py
@@ -2,3 +2,12 @@ def multiply_numbers(a, b):
     return a * b
 def add_numbers(a, b):
     return a + b
+def factorial(n):
+    if n < 0:
+        raise ValueError("Факториал определён только для неотрицательных чисел.")
+    if n == 0 or n == 1:
+        return 1
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
