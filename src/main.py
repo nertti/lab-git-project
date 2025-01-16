ee9b6e4787393573c6fcea383c6b38bccf8fbe2f
Daniil Neretin <dneretin01@gmail.com>
2025-01-16 23:07:07 +0300

    add feature/main_logic

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from utils import add_numbers, multiply_numbers
+
+def main():
+    x, y = 5, 10
+    print(f"Сумма чисел {x} и {y}: {add_numbers(x, y)}")
+    print(f"Произведение чисел {x} и {y}: {multiply_numbers(x, y)}")
+
+if __name__ == "__main__":
+    main()
