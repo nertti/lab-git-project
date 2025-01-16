7c1d60472d32df097d3f869a4680a6634bf64063
Daniil Neretin <dneretin01@gmail.com>
2025-01-16 23:07:46 +0300

    fix text in main.py

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ from utils import add_numbers, multiply_numbers
 
 def main():
     x, y = 5, 10
-    print(f"Сумма чисел {x} и {y}: {add_numbers(x, y)}")
-    print(f"Произведение чисел {x} и {y}: {multiply_numbers(x, y)}")
+    print(f"Сумма чисел - {x} и {y}: {add_numbers(x, y)}")
+    print(f"Произведение чисел - {x} и {y}: {multiply_numbers(x, y)}")
 
 if __name__ == "__main__":
     main()
